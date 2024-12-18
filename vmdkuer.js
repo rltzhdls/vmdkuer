cbb644d2b68c30b70611f3d48f33158f883f8b8c
rltzhdls <136443821+rltzhdls@users.noreply.github.com>
2024-12-18 22:24:10 +0900

    Add files via upload

---
+++ vmdkuer.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+	if (window.location.pathname === '/' || window.location.pathname === '/index.html') {
+    var vds = document.getElementById('vds');
+    
+    if (vds) {
+        vds.src = "https://cdn.jsdelivr.net/gh/rltzhdls/vmvkdks@main/akma.mp4";
+        document.getElementById('vd').load();
+    }
+	}
+});
+
